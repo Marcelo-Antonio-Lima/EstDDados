a0f919a9f6c26a88b085d699b654732f6feabb43
Marcelo-Antonio-Lima <84820723+Marcelo-Antonio-Lima@users.noreply.github.com>
2022-09-28 12:23:57 -0300

    Add files via upload

---
+++ Exercicios_Estruturas_de_dados/src/exerciciosRecursivos/ContadoresRecursivos.java
@@ -0,0 +1,127 @@
+package exerciciosRecursivos;
+
+/**
+ * 
+ * Contadores com 1 variável
+ *
+ */
+public class ContadoresRecursivos {
+
+	/**
+	 * Chama o método {@link #contar_10_1V_cresc(int)} e retorna 10
+	 * 
+	 * @return retorna 10 ao terminar de contar
+	 */
+
+	public static int contar_10_1V_cresc() {
+		return contar_10_1V_cresc(0);
+	}
+
+	/**
+	 * Contador recursivo que conta de 0 até 10
+	 * 
+	 * @param contador Variável que serve de contador
+	 * @return Retorna 10 ao terminar de contar ou chama a função de forma recursiva
+	 *         com contador recebendo +1
+	 */
+	public static int contar_10_1V_cresc(int contador) {
+
+		if (contador == 10) {
+			return contador;
+		} else {
+			System.out.println(contador++);
+			return contar_10_1V_cresc(contador);
+		}
+	}
+
+	/**
+	 * Chama o método {@link #contar_10_1V_decresc(int)} e retorna 0
+	 * 
+	 * @return retorna 0 ao terminar de contar
+	 */
+	public static int contar_10_1V_decresc() {
+		return contar_10_1V_decresc(10);
+
+	}
+
+	/**
+	 * Contador recursivo que conta de 10 até 0
+	 * 
+	 * @param contador Variável que serve de contador
+	 * @return Retorna 0 ao terminar de contar ou chama a função de forma recursiva
+	 *         com contador recebendo -1
+	 */
+	private static int contar_10_1V_decresc(int contador) {
+
+		if (contador == 0) {
+			return contador;
+		} else {
+			System.out.println(contador--);
+			return contar_10_1V_decresc(contador);
+		}
+	}
+
+	/**
+	 * Chama o método {@link #contador_2V_cresc(int controle, int contador)}
+	 * controle, int contador) e retorna 10
+	 * 
+	 * @return retorna 10 ao terminar de contar
+	 */
+	public static int contar_10_2V_cresc() {
+		return contador_2V_cresc(0, 10);
+	}
+
+	/**
+	 * Contador crescente recursivo com 2 variáveis
+	 * 
+	 * @param contador Variável que serve de contador
+	 * @param controle Variável de controle
+	 * @return Retorna contador ao terminar de contar ou chama a função de forma
+	 *         recursiva com o contador recebendo +1
+	 */
+
+	public static int contador_2V_cresc(int contador, int controle) {
+		if (contador > controle) {
+			System.out.println("Número passado " + contador + " É maior que o valor alvo");
+			return contador;
+		}
+		if (contador == controle) {
+			return contador;
+		} else {
+			System.out.println(contador);
+			return contador_2V_cresc(contador + 1, controle);
+		}
+	}
+
+	/**
+	 * Chama o método {@link #contar_2V_decresc(int, int)} e retorna 0
+	 * 
+	 * @return retorna 0 ao terminar de contar
+	 */
+	public static int contar_10_2V_decresc() {
+		return contar_2V_decresc(10, 0);
+
+	}
+
+	/**
+	 * Contador com 2 variáveis decrescente
+	 * 
+	 * 
+	 * @param contador Variável que serve de contador
+	 * @param controle Variável de controle
+	 * @return Retorna controle ao terminar de contar ou chama a função de forma
+	 *         recursiva com controle recebendo -1
+	 */
+	public static int contar_2V_decresc(int contador, int controle) {
+		if (contador < controle) {
+			System.out.println("Número passado " + contador + " é menor que o valor alvo");
+			return contador;
+		}
+		if (contador == controle) {
+			return contador;
+		} else {
+			System.out.println(contador);
+			return contar_2V_decresc(contador - 1, controle);
+		}
+	}
+}
---
+++ Exercicios_Estruturas_de_dados/src/exerciciosRecursivos/Permutacoes.java
@@ -0,0 +1,114 @@
+package exerciciosRecursivos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutacoes {
+
+	public static void permutacoesString(String palavra) {
+		char[] palavraChars = palavra.toCharArray();
+		List<String> listaPermutacoes = new ArrayList<>();
+		permutacoesString(palavraChars, 0, 0, 0, listaPermutacoes);
+	}
+
+	private static void permutacoesString(char[] palavra, int contadorPrincipal, int contadorAuxiliar,
+			int contadorPermutacoes, List<String> listaPermutacoes) {
+		char[] palavraTrabalho = palavra.clone();
+		if (contadorPermutacoes == palavra.length) {
+			listaPermutacoes.sort(null);
+			imprimirPermutacoes(listaPermutacoes, fatorial(palavra.length) / palavra.length);
+		} else {
+			if (contadorPrincipal == palavra.length) {
+				palavraTrabalho = moverChar(palavraTrabalho);
+				permutacoesString(palavraTrabalho, 0, 0, contadorPermutacoes + 1, listaPermutacoes);
+			} else {
+				if (contadorAuxiliar == palavra.length) {
+
+					permutacoesString(palavra, contadorPrincipal + 1, 0, contadorPermutacoes, listaPermutacoes);
+				} else {
+					palavraTrabalho = letraTroca(palavraTrabalho, contadorPrincipal, contadorAuxiliar);
+					System.out.println(String.valueOf(palavraTrabalho));
+					if (listaPermutacoes.contains(String.valueOf(palavraTrabalho))) {
+						permutacoesString(palavra, contadorPrincipal, contadorAuxiliar + 1, contadorPermutacoes,
+								listaPermutacoes);
+
+					} else {
+						listaPermutacoes.add(String.valueOf(palavraTrabalho));
+						permutacoesString(palavra, contadorPrincipal, contadorAuxiliar + 1, contadorPermutacoes,
+								listaPermutacoes);
+					}
+
+				}
+			}
+		}
+	}
+
+	private static char[] moverChar(char[] palavra) {
+		char[] retorno = new char[palavra.length];
+		for (int i = 0; i < palavra.length; i++) {
+			if (i == 0) {
+				retorno[retorno.length - 1] = palavra[i];
+			} else {
+				retorno[i - 1] = palavra[i];
+			}
+		}
+		return retorno;
+	}
+
+	private static void imprimirPermutacoes(List<String> lista, int nLinhas) {
+		for (int i = 0; i < nLinhas; i++) {
+			for (int j = 0; j < lista.size() / nLinhas; j++) {
+				System.out.print(lista.get(i + j * nLinhas) + " ");
+			}
+			System.out.println("");
+		}
+	}
+
+	private static int fatorial(int numero) {
+		if (numero <= 1) {
+			return 1;
+		} else {
+			return numero * fatorial(numero - 1);
+		}
+	}
+
+	private static char[] letraTroca(char[] palavra, int indexTroca, int indexAlvo) {
+		char[] retorno = palavra.clone();
+		char letraFixa, letraTroca;
+		letraFixa = retorno[indexTroca];
+		letraTroca = retorno[indexAlvo];
+		retorno[indexAlvo] = letraFixa;
+		retorno[indexTroca] = letraTroca;
+		return retorno;
+
+	}
+
+	public static void permutacoes2(String palavra) {
+		char[] palavraChars = palavra.toCharArray();
+		permutacoes2(palavraChars, "");
+	}
+
+	private static void permutacoes2(char[] palavra, String letraAnterior) {
+		String palavraAnterior =letraAnterior + String.valueOf(palavra);
+		String palavraEnvio =letraAnterior + palavra[0];
+		System.out.println(palavraAnterior);
+		
+		if (palavra.length > 2) {
+			permutacoes2(removerPrimeiroChar(palavra),palavraEnvio );
+			palavra = moverChar(palavra);
+			permutacoes2(removerPrimeiroChar(palavra), palavraEnvio);
+		}
+		
+		
+
+	}
+
+	private static char[] removerPrimeiroChar(char[] palavra) {
+		char[] retorno = new char[palavra.length - 1];
+		for (int i = 1; i < palavra.length; i++) {
+			retorno[i - 1] = palavra[i];
+		}
+		return retorno;
+	}
+
+}
